58b0bf0ffc6831a94be0a10f4e92c1544e1d39d7
yongbin <rhdydqls1024@naver.com>
2023-01-14 15:21:32 +0900

    :seedling: :: initial commit

---
+++ src/main/java/com/girlfriend/sunghun/SunghunApplication.java
@@ -0,0 +1,13 @@
+package com.girlfriend.sunghun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SunghunApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SunghunApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/girlfriend/sunghun/SunghunApplicationTests.java
@@ -0,0 +1,13 @@
+package com.girlfriend.sunghun;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SunghunApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
